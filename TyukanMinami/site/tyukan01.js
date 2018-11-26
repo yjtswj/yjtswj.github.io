ff349ecff433b2b304fc0de26cc4232020ce1e24
yjtswj <m011442061@edu.teu.ac.jp>
2018-11-26 18:03:31 +0900

    これでどうだぁ（画像の表示）
    おりやぁあ（画像の表示）

--- TyukanMinami/site/tyukan01.js
+++ TyukanMinami/site/tyukan01.js
@@ -1,3 +1,4 @@
+/*
 
 //fetch(url).then(function).then(function)....
 
@@ -19,6 +20,28 @@ fetch("http://morimorihoge.github.io/teu2018f/tweets.json").then(
       });
       document.querySelector("#answer").innerHTML = answerHtml;
       document.querySelector("#imagg").innerHTML = imggHtml;
+
+    }
+
+
+);*/
+
+
+fetch("http://morimorihoge.github.io/teu2018f/tweets.json").then(
+    function (response) {
+      return response.json();
+    }
+).then(
+    function (json) {
+      let answerHtml = "";
+      let imggHtml = "";
+      json.forEach(function (tweet) {
+        imggHtml = tweet.avatar;
+        answerHtml += `<li><img src="${tweet.avatar}" width="100">${tweet.name}</li>`;
+        answerHtml += tweet.message;
+        answerHtml += tweet.tweetedAt;
+      });
+      document.querySelector("#answer").innerHTML = answerHtml;
 
     }
 
