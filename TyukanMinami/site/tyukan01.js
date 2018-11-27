86bd46a79fa31f32014c670359f7373c954d3d95
yjtswj <m011442061@edu.teu.ac.jp>
2018-11-27 14:49:00 +0900

    1127
    1127

--- TyukanMinami/site/tyukan01.js
+++ TyukanMinami/site/tyukan01.js
@@ -1,31 +1,4 @@
-/*
-
 //fetch(url).then(function).then(function)....
-
-fetch("http://morimorihoge.github.io/teu2018f/tweets.json").then(
-  function (response) {
-    return response.json();
-  }
-).then(
-    function (json) {
-      let answerHtml = "";
-      let imggHtml = "";
-      json.forEach(function (tweet) {
-        answerHtml += `<li>${tweet.name}</li>`;
-        answerHtml += `${tweet.message}`;
-        answerHtml += `${tweet.tweetedAt}`;
-        imggHtml += `${tweet.avatar}`;
-
-
-      });
-      document.querySelector("#answer").innerHTML = answerHtml;
-      document.querySelector("#imagg").innerHTML = imggHtml;
-
-    }
-
-
-);*/
-
 
 fetch("http://morimorihoge.github.io/teu2018f/tweets.json").then(
     function (response) {
@@ -37,7 +10,8 @@ fetch("http://morimorihoge.github.io/teu2018f/tweets.json").then(
       let imggHtml = "";
       json.forEach(function (tweet) {
         imggHtml = tweet.avatar;
-        answerHtml += `<li><img src="${tweet.avatar}" width="100">${tweet.name}</li>`;
+        answerHtml += `<li><img src="${tweet.avatar}" width="100"></li>`
+        answerHtml += `<li>${tweet.name}</li>`;
         answerHtml += tweet.message;
         answerHtml += tweet.tweetedAt;
       });
